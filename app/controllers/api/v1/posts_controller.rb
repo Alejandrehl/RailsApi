7dffd4b66fbd37e66478784f25ecceb4da2d3ae1
Alejandro <Alejandrehl@icloud.com>
2018-06-18 15:26:47 -0400

    First commit

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,11 @@
+module Api
+    module V1
+        class PostsController < ApplicationController
+
+            def index
+                @posts = Post.order('created_at DESC')
+            end
+            
+        end
+    end
+end
